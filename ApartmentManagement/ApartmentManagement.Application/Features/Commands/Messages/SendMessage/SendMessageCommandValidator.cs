410b9a99e9726db8fc8bb92e15e70df70c57ce88
Ahmet Senlik <ahmtsenlik@gmail.com>
2022-02-19 00:46:48 +0300

    Updated SendMessage Validator

--- ApartmentManagement/ApartmentManagement.Application/Features/Commands/Messages/SendMessage/SendMessageCommandValidator.cs
+++ ApartmentManagement/ApartmentManagement.Application/Features/Commands/Messages/SendMessage/SendMessageCommandValidator.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace ApartmentManagement.Application.Features.Commands.Messages.SendMessage
 {
-    public class SendMessageCommandValidator:AbstractValidator<SendMessageCommandRequest>
+    public class SendMessageCommandValidator : AbstractValidator<SendMessageCommandRequest>
     {
+        public SendMessageCommandValidator()
+        {
+            RuleFor(c => c.SenderId).NotEmpty().WithMessage("{Sender Id} is required.");
+
+            RuleFor(c => c.ReceiverId).NotEmpty().WithMessage("{Receiver Id} is required.");
+
+            RuleFor(c => c.Title).NotEmpty().WithMessage("{Title} is required.")
+                .MaximumLength(256).WithMessage("{Title} can be a maximum of 256 characters.");
+
+            RuleFor(c => c.Content).NotEmpty().WithMessage("{First Name} is required.");
+    }
     }
 }
